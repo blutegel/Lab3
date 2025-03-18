dd5a5d7b80feee7f7a48d001747b943851ecc8bd
James <blutegel@users.noreply.github.com>
2025-03-18 11:00:29 -0700

    empty menu

--- src/MenuSelection.java
+++ src/MenuSelection.java
@@ -2,6 +2,7 @@
  * Partner Lab3
  * Description Program to allow for a menu selection of the two methods and an option for exit.
  * @author James Rohr
+ * @author Asma Klai
  * @since 3-18-25
  * @param menuSelection
  * @return method chosen
